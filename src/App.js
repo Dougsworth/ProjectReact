176242fdef6637e7a63d41d75731b8ecf16c8d95
Douglas Byfield <dougyd30@gmail.com>
2021-01-27 15:10:41 -0500

    trying sumn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Tweet from "./Tweet";
 
 function App() {
+ const [users, setUsers] = useState([
+   {name: "Ed", message: 'Hello there'},
+   {name: "Dougy", message: 'Uwi is life changing'},
+   {name: "Yeseka", message: 'I owe dougy a movie date'}
+ ]);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    {users.map(user => (
+      <Tweet name={user.name} message = {user.message} />
+     ))}
     </div>
   );
 }
